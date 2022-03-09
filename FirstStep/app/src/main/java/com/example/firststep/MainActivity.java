05254dab7eb81ae0df4f4bfc0d8ce61ca955dee5
EvilAv <andrey.vookolov85@gmail.com>
2022-03-10 02:24:24 +0300

    mbedtls

--- FirstStep/app/src/main/java/com/example/firststep/MainActivity.java
+++ FirstStep/app/src/main/java/com/example/firststep/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     // Used to load the 'firststep' library on application startup.
     static {
         System.loadLibrary("firststep");
+        System.loadLibrary("mbedcrypto");
     }
 
     private ActivityMainBinding binding;
@@ -23,9 +24,21 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        int res = initRng();
+        byte[] rnd = randomBytes(10);
+
+        byte[] key = "rnd[0]".getBytes();
+        byte[] enc = encrypt(key, "Test".getBytes());
+
+        String out = new String(decrypt(key, enc));
+
         // Example of a call to a native method
         TextView tv = binding.sampleText;
+        //tv.setText(out);
         tv.setText(stringFromJNI());
+
+        TextView test = findViewById(R.id.rnd_elem);
+        test.setText("Some random numbers: "+Byte.toString( rnd[0])+" , "+String.format("%d", rnd[1]));
     }
 
     /**
@@ -33,4 +46,8 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int no);
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
 }
