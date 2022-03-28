d472c477660836160f3bb752aae002ce9328eaf4
EvilAv <andrey.vookolov85@gmail.com>
2022-03-28 22:28:55 +0300

    pinpadactivity

--- FirstStep/app/src/main/java/com/example/firststep/MainActivity.java
+++ FirstStep/app/src/main/java/com/example/firststep/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.firststep;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.firststep.databinding.ActivityMainBinding;
 
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityResultLauncher activityResultLauncher;
 
     // Used to load the 'firststep' library on application startup.
     static {
@@ -25,22 +38,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         int res = initRng();
-        byte[] rnd = randomBytes(10);
 
-        byte[] key = "rnd[0]".getBytes();
-        byte[] enc = encrypt(key, "Test".getBytes());
+        activityResultLauncher  = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        // обработка результата
+                        String pin = data.getStringExtra("pin");
+                        Toast.makeText(MainActivity.this, pin,
+                                Toast.LENGTH_SHORT).show();  // lasts 2 seconds (so loooooong)
+                        byte[] rnd = randomBytes(10);
+                        TextView test = findViewById(R.id.rnd_elem);
+                        test.setText("Some random numbers: "+Byte.toString( rnd[0])+" , "+String.format("%d", rnd[1]));
+                    }
+                });
 
-        String out = new String(decrypt(key, enc));
-
-        // Example of a call to a native method
-        TextView tv = binding.sampleText;
-        //tv.setText(out);
-        tv.setText(stringFromJNI());
-
-        TextView test = findViewById(R.id.rnd_elem);
-        test.setText("Some random numbers: "+Byte.toString( rnd[0])+" , "+String.format("%d", rnd[1]));
     }
 
+    public void onButtonClick(View v)
+    {
+
+        Intent it = new Intent(this, PinpadActivity.class);
+        activityResultLauncher.launch(it);
+    }
+
+    public static byte[] stringToHex(String s)
+    {
+        byte[] hex;
+        try
+        {
+            hex = Hex.decodeHex(s.toCharArray());
+        }
+        catch (DecoderException ex)
+        {
+            hex = null;
+        }
+        return hex;
+    }
     /**
      * A native method that is implemented by the 'firststep' native library,
      * which is packaged with this application.
